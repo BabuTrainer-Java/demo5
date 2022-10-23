436313707db4cdafa5cedbe41d361f3b25eae5f5
babu <babu@gmail.com>
2022-10-24 01:15:45 +0530

    HaiSpringBoot

---
+++ demo1501/src/main/java/demo1501/Demo1501Application.java
@@ -0,0 +1,13 @@
+package demo1501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1501Application.class, args);
+	}
+
+}
---
+++ demo1501/src/main/java/demo1501/HelloController.java
@@ -0,0 +1,16 @@
+package demo1501;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+@RequestMapping("/hai")
+@ResponseBody
+public String sayHello()
+{
+return "Hai  Springboot";	
+}
+	
+}
